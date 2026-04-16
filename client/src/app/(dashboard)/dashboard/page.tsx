aa9a3873d73ec8f2a997975767cc1c0d75f41d59
psoumojit40 <psoumojit40@gmail.com>
2026-04-17 00:00:56 +0530

    changes made to manager dashboard, calender

--- client/src/app/(dashboard)/dashboard/page.tsx
+++ client/src/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState, useMemo } from 'react'; 
+import { useEffect, useState, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState, AppDispatch } from '@/store';
 import { fetchLeaveData, fetchLeaveSettings } from '@/store/leaveSlice'; 
